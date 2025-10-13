c8eb129fee4a308f4e4698374bfd41e008d54a98
jp <joepettinelli7@gmail.com>
2025-10-13 15:35:11 -0400

    Initial Commit

---
+++ zero-turn-gameTests/zero_turn_gameTests.swift
@@ -0,0 +1,17 @@
+//
+//  zero_turn_gameTests.swift
+//  zero-turn-gameTests
+//
+//  Created by Joe Pettinelli on 10/13/25.
+//
+
+import Testing
+@testable import zero_turn_game
+
+struct zero_turn_gameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ zero-turn-gameUITests/zero_turn_gameUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  zero_turn_gameUITestsLaunchTests.swift
+//  zero-turn-gameUITests
+//
+//  Created by Joe Pettinelli on 10/13/25.
+//
+
+import XCTest
+
+final class zero_turn_gameUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
